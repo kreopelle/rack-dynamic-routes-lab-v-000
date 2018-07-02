73679459c013176f7b30b3eade9af7ad32627a89
Kayla Reopelle <kreopelle@gmail.com>
2018-07-02 16:38:34 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = Items.find{|i| i.name = item_name}
+      item.price
 
     resp.finish
   end
