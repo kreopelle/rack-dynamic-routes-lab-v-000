1e5b7fe68f2c99f5368700740c09fe5da63590e1
Kayla Reopelle <kreopelle@gmail.com>
2018-07-02 17:03:30 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      item = Item.find{|i| i.name = item_name}
+      item = @@items.find{|i| i.name = item_name}
       if !item
         resp.write "Item not found"
         resp.status = 400
